948042eb9876a2aa5e93ae42216f78185c12aec1
Priya <priyanageofficial@gmail.com>
2024-02-18 02:02:32 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [EmployeesController::class, 'index'])->name('index');
-
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
